91ee0c35364d8bb50405283c133ea45be40a7212
Graziella Custódio <graziella_mc@hotmail.com>
2026-08-15 19:55:39 +0100

    Base structure of the homepage

--- pages/index.php
+++ pages/index.php
@@ -3,9 +3,9 @@
 
 <?php include_once "../components/cp_head.php"?>
 
-<body class="bg-pink">
+<body class="overflow-x-hidden bg-pink box-border">
     <div class="relative">
-        <div class="bg-darkpurple/80 backdrop-brightness-200 h-12 w-60 absolute left-8 top-12 content-center rounded-xl">
+        <div class="bg-darkpurple/80 backdrop-brightness-200 h-12 w-60 absolute left-3 top-12 content-center rounded-xl">
             <div class="text-center font-poppins uppercase font-bold text-neonyellow text-2xl">top receitas</div>
         </div>
         <img class="w-full" src="../images/fundo_top_sm.png" alt="">
@@ -21,10 +21,100 @@
         <div class="bg-darkpurple h-40"></div>
     </div>-->
 
-    <div>
-        <div class="font-poppins text-neonyellow text-xl font-semibold uppercase">Receitas da época</div>
-        <div class="font-lily">Summertime!</div>
-    </div>
+    <section class="py-5">
+        <div class="px-5">
+            <div class="font-poppins text-neonyellow text-md font-semibold uppercase italic">Receitas da época</div>
+            <div class="font-lily text-4xl text-neonyellow text-shadow-2xl text-shadow-darkpurple-500/20 pl-6">Summertime!</div>
+        </div>
+
+        <div class="my-4 ml-5 flex gap-4 overflow-scroll">
+            <div class="relative">
+                <div class="absolute left-3">
+                    <div class="text-center text-sm px-2 py-2 font-poppins font-semibold text-neonyellow bg-darkpurple w-30 rounded-lg">Caipirinha</div>
+                </div>
+                <div class="pt-4">
+                    <div class="bg-neonyellow w-36 h-36 rounded-md"></div>
+                    <!-- aqui vai ser uma imagem no lugar da div -->
+                </div>
+            </div>
+            <div class="relative">
+                <div class="absolute left-3">
+                    <div class="text-center text-sm px-2 py-2 font-poppins font-semibold text-neonyellow bg-darkpurple w-30 rounded-lg">Caipirinha</div>
+                </div>
+                <div class="pt-4">
+                    <div class="bg-neonyellow w-36 h-36 rounded-md"></div>
+                    <!-- aqui vai ser uma imagem no lugar da div -->
+                </div>
+            </div>
+            <div class="relative">
+                <div class="absolute left-3">
+                    <div class="text-center text-sm px-2 py-2 font-poppins font-semibold text-neonyellow bg-darkpurple w-30 rounded-lg">Caipirinha</div>
+                </div>
+                <div class="pt-4">
+                    <div class="bg-neonyellow w-36 h-36 rounded-md"></div>
+                    <!-- aqui vai ser uma imagem no lugar da div -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="flex relative bg-darkpurple h-72">
+        <div class="custom-shape-divider-top-1786813579">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 200" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+        <div class="m-auto w-full mx-5 self-auto bg-darkerpurple h-32 text-center content-center rounded-xl font-poppins text-neonyellow">
+            <p class="my-1 font-medium text-sm italic">Ainda sem inspiração?</p>
+            <a class="my-1 font-semibold text-md">Veja o nosso catálogo!</a>
+        </div>
+        <div class="custom-shape-divider-bottom-1786814249">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 200" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+    </section>
+
+    <section class="bg-mediumpurple py-5">
+        <div class="px-5">
+            <div class="font-poppins text-neonyellow text-md font-semibold uppercase italic">Na dúvida, veja as</div>
+            <div class="font-lily text-2xl text-neonyellow text-shadow-2xl text-shadow-darkpurple-500/20 pl-6">Escolhas dos editores</div>
+        </div>
+        <div class="columns-2 gap-4 my-4">
+            <div class="relative aspect-2/5">
+                <div class="absolute left-3">
+                    <div class="text-center text-sm px-2 py-2 font-poppins font-semibold text-neonyellow bg-darkpurple w-30 rounded-lg">Caipirinha</div>
+                </div>
+                <div class="pt-4">
+                    <div class="bg-neonyellow w-full h-90 rounded-md"></div>
+                    <!-- aqui vai ser uma imagem no lugar da div -->
+                </div>
+            </div>
+            <div class="aspect-2/5">
+                <div class="relative mb-4">
+                    <div class="absolute left-3">
+                        <div class="text-center text-sm px-2 py-2 font-poppins font-semibold text-neonyellow bg-darkpurple w-30 rounded-lg">Caipirinha</div>
+                    </div>
+                    <div class="pt-4">
+                        <div class="bg-neonyellow w-full h-26 rounded-md"></div>
+                        <!-- aqui vai ser uma imagem no lugar da div -->
+                    </div>
+                </div>
+                <div class="relative mb-4">
+                    <div class="absolute left-3">
+                        <div class="text-center text-sm px-2 py-2 font-poppins font-semibold text-neonyellow bg-darkpurple w-30 rounded-lg">Caipirinha</div>
+                    </div>
+                    <div class="pt-4">
+                        <div class="bg-neonyellow w-full h-26 rounded-md"></div>
+                        <!-- aqui vai ser uma imagem no lugar da div -->
+                    </div>
+                </div>
+                <div class="relative">
+                    <div class="bg-neonyellow w-full h-26 rounded-md"></div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
 
