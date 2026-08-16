c4e59399dd4a4f89cd4f597e5268489f5b2cb3dc
Gabriel Azevedo <gabrilazevedo@outlook.pt>
2026-08-16 15:07:28 +0100

    catálogo mobile feito + footer mobile feito

--- pages/catalogo.php
+++ pages/catalogo.php
@@ -57,27 +57,6 @@
         </section>
     </main>
 
-    <footer class="bg-[#200322] border-t-2 border-dashed mt-10 py-6">
-        <div class="border-b-1 ">
-            <img  src="../images/logoCompleta.svg" class="mx-auto w-16">
-            <div class="flex justify-center my-6">
-                <img src="../images/instaAmarelo.svg" class="px-3">
-                <img src="../images/tiktokAmarelo.svg" class="px-3">
-                <img src="../images/pinterestAmarelo.svg" class="px-3">
-            </div>
-        </div>
+    <?php include_once "../components/cp_footer.php"?>
 
-        <div class="text-center my-6">
-            <p class="pb-6">Contacte-nos</p>
-            <p class="pb-6">Perguntas Frequentes</p>
-            <p class="pb-6">Sobre Nós</p>
-            <p>Voltar ao Topo</p>
-        </div>
-
-        <div class="text-xs font-extralight text-center" style="">
-            <p>© 2024-2026 O Alquimista. All rights reserved.</p>
-            <p>Gabriel Azevedo | Graziella Custódio | Nicole Santos</p>
-        </div>
-
-    </footer>
 </body>
