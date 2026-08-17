0ef7710e1642944c3c17564db50ab93db88d72ea
Graziella Custódio <graziella_mc@hotmail.com>
2026-08-17 17:29:31 +0100

    About us page done

---
+++ pages/sobrenosNoite.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once "../components/cp_head.php"?>
+
+<body class="bg-pink pt-10">
+
+    <section class="px-5
+                    sm:grid sm:grid-cols-2
+                    md:px-12
+                    lg:px-30
+                    xl:px-52">
+
+        <div class="sm:col-span-1 content-center">
+            <div class="font-lily text-neonyellow text-2xl
+                    md:text-3xl
+                    lg:text-4xl
+                    2xl:text-6xl">Seja muito bem-vindo!</div>
+            <div class="font-poppins text-neonyellow text-md mb-5
+                    md:text-lg
+                    lg:text-xl
+                    xl:text-2xl
+                    2xl:text-3xl">Somos os criadores do Alquimista!</div>
+        </div>
+        <img class="w-5/6 m-auto sm:col-span-1" src="../images/bemVindoNoite.png">
+
+    </section>
+
+    <section class="flex relative bg-darkpurple">
+        <svg class="absolute w-full" viewBox="0 0 1920 284" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1920 0C1769.5 169 1537.5 179.5 1336 179.5C1134.5 179.5 840 84 584 84C328 84 67.5 176 0 284V0H1920Z" fill="#A94064"/>
+        </svg>
+
+        <div class="mx-auto my-20 text-center content-center
+                    sm:my-32
+                    md:my-40
+                    lg:my-48
+                    xl:my-56
+                    2xl:my-72">
+            <p class="my-1 text-sm px-5 font-poppins text-neonyellow
+                      md:text-md md:px-12
+                      lg:text-lg lg:px-30
+                      xl:text-xl xl:px-52
+                      2xl:text-2xl">O Alquimista foi criado em 2024 pela união de três amigos — Nicole, Gabriel e Graziella — como um trabalho escolar no âmbito da unidade curricular de Laboratório de Multimédia I, na Universidade de Aveiro. Inicialmente concebido apenas como um projeto experimental, foi mais tarde desenvolvido como um site funcional, já na unidade de Laboratório de Multimédia II, tendo sido levado a cabo por Graziella e Gabriel.
+                Mantendo sempre um estilo único e Irreverente é uma alternativa divertida e funcional a sites convencionais de bebidas, tendo como objetivo facilitar a pesquisa e partilha de receitas de cocktails.
+            </p>
+        </div>
+        <svg class="w-full absolute bottom-0" viewBox="0 0 1920 284" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1920 284H0C103 101 377 84 584 84C791 84 1115.5 179.5 1336 179.5C1556.5 179.5 1789 177 1920 0V284Z" fill="#732253"/>
+        </svg>
+    </section>
+
+    <section class="bg-mediumpurple px-5
+                    sm:grid sm:grid-cols-2
+                    md:px-12
+                    lg:px-30
+                    xl:px-52">
+
+        <div class="sm:col-span-1 sm:order-2 sm:content-center mb-5">
+            <div class="font-poppins text-neonyellow text-md
+                    sm:text-end
+                    md:text-lg
+                    lg:text-xl
+                    xl:text-2xl
+                    2xl:text-3xl">Necessitas de algo?</div>
+            <div class="font-lily text-neonyellow text-2xl mb-4
+                    sm:text-end
+                    md:text-3xl
+                    lg:text-4xl lg:mb-5
+                    2xl:text-6xl">Contacte-nos!</div>
+            <div class="font-poppins text-neonyellow text-sm text-center mb-4
+                    sm:text-end
+                    md:text-md
+                    lg:text-lg
+                    xl:text-xl lg:mb-5
+                    2xl:text-2xl">osalquimistas@gmail.com</div>
+            <div class="sm:flex sm:justify-end gap-5 sm:gap-2">
+                <div class="flex gap-3 justify-center mb-2
+                            sm:justify-end sm:gap-2">
+                    <img class="w-5" src="../images/instaAmarelo.svg">
+                    <img class="w-5" src="../images/tiktokAmarelo.svg">
+                    <img class="w-5" src="../images/pinterestAmarelo.svg">
+                </div>
+                <p class="font-poppins text-neonyellow text-sm text-center
+                        sm:text-end
+                        md:text-md
+                        lg:text-lg
+                        xl:text-xl
+                        2xl:text-2xl">@o_alquimista</p>
+            </div>
+        </div>
+
+        <img class="w-9/10 m-auto
+                    sm:col-span-1 sm:order-1 sm:w-full" src="../images/contacteNoite.png">
+
+    </section>
+
+</body>
+</html>
